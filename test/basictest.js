42acc6455a9e2c97b950f72bc1914a2edfa21815
Will Colton <will.colton@hp.com>
2014-05-04 19:25:38 -0400

    Added a few negative tests and new positive tests.

--- test/basictest.js
+++ test/basictest.js
@@ -3,21 +3,80 @@ var request = require('supertest'),
 
 require('../lib/losdos')(app, { verbose: true, routes: __dirname + '/routes' });
 
-describe('GET /api/stuff', function(){
-  it('respond with json', function(done){
-    request(app)
-      .get('/api/stuff')
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200, done);
-  })
+describe('routes/stuff/mockstuff.js#get', function(){
+    describe('GET /api/stuff', function(){
+        it('should respond with json', function(done){
+            request(app)
+                    .get('/api/stuff')
+                    .set('Accept', 'application/json')
+                    .expect('Content-Type', /json/)
+                    .expect(200, done);
+        });
+    });
+
+    describe('GET /stuff', function(){
+        it('should NOT be found', function(done){
+            request(app)
+                    .get('/stuff')
+                    .expect(404, done);
+        });
+    });
+
+    describe('GET /api/stuff/get', function(){
+        it('should NOT be found', function(done){
+            request(app)
+                    .get('/api/stuff/get')
+                    .expect(404, done);
+        });
+    });
+
+    describe('GET /stuff/get', function(){
+        it('should NOT be found', function(done){
+            request(app)
+                    .get('/stuff/get')
+                    .expect(404, done);
+        });
+    });
 });
 
-describe('GET /stuff/view', function(){
-  it('respond with json', function(done){
-    request(app)
-      .get('/stuff/view')
-	  .expect('hello')
-      .expect(200, done);
-  })
+describe('routes/stuff/mockstuff.js#view', function(){
+    it('should respond with expected content', function(done){
+        request(app)
+                .get('/stuff/view')
+                .expect('routes/stuff/mockstuff.js#view')
+                .expect(200, done);
+    })
+});
+
+describe('routes/stuff/mockstuff.js#put', function(){
+    describe('PUT /api/stuff', function(){
+        it('should respond with json', function(done){
+            request(app)
+                    .put('/api/stuff')
+                    .set('Accept', 'application/json')
+                    .expect('Content-Type', /json/)
+                    .expect({route: 'routes/stuff/mockstuff.js#put.index', success: true})
+                    .expect(200, done);
+        });
+
+        it('should respond with json with a slash', function(done){
+            request(app)
+                    .put('/api/stuff/')
+                    .set('Accept', 'application/json')
+                    .expect('Content-Type', /json/)
+                    .expect({route: 'routes/stuff/mockstuff.js#put.index', success: true})
+                    .expect(200, done);
+        });
+    });
+
+    describe('PUT /api/stuff/otherstuff', function(){
+        it('should respond with json', function(done){
+            request(app)
+                    .put('/api/stuff/otherstuff')
+                    .set('Accept', 'application/json')
+                    .expect('Content-Type', /json/)
+                    .expect({route: 'routes/stuff/mockstuff.js#put.otherstuff', success: true})
+                    .expect(200, done);
+        });
+    });
 });
--- test/routes/stuff/mockstuff.js
+++ test/routes/stuff/mockstuff.js
@@ -1,7 +1,16 @@
 exports.get = function(req, res) {
-	res.json({id: 'stuff1'})
+    res.json({route: 'routes/stuff/mockstuff.js#put.index'});
 };
 
 exports.view = function(req, res) {
-	res.send('hello');
-}
+    res.send('routes/stuff/mockstuff.js#view');
+};
+
+exports.put = {
+    index: function(req, res) {
+        res.json({route: 'routes/stuff/mockstuff.js#put.index', success: true});
+    },
+    otherstuff: function(req, res) {
+        res.json({route: 'routes/stuff/mockstuff.js#put.otherstuff', success: true});
+    }
+};
