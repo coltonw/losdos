560d8f6848320589deef55a1c76b2bdbeede35ea
Will Colton <will.colton@hp.com>
2014-05-17 20:07:14 -0400

    Another attempt at coveralls.

--- coveralls.sh
+++ coveralls.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-sh node_modules/.bin/mocha -R mocha-lcov-reporter > coverage.lcov
-cat coverage.lcov | sh node_modules/.bin/coveralls
+cd ${0%/*}
+echo "running coveralls script from:"
+pwd
+./node_modules/.bin/mocha -R mocha-lcov-reporter > coverage.lcov
+cat coverage.lcov | node ./node_modules/coveralls/bin/coveralls.js
 exit 0
